a8b80d688e136c71afb191927a4dac665f43f26b
gardenagencies <officialgardenagencies@gmail.com>
2025-06-07 18:13:22 +0530

    Create authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && user.password === password) {
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+});
